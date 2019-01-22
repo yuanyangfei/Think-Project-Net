f4bc5e42cb2e822aa7e7ee495c49a04445f570b8
yuanyangfei <1207768558@qq.com>
2019-01-22 11:34:05 +0800

    log

---
+++ Think.Code/Log/Log.cs
@@ -0,0 +1,35 @@
+﻿/*******************************************************************************
+ * Copyright © 2016 Think.Framework 版权所有
+ * Author: Think
+ * Description: Think快速开发平台
+ * Website：http://www.Think.cn
+*********************************************************************************/
+using log4net;
+
+namespace Think.Code
+{
+    public class Log
+    {
+        private ILog logger;
+        public Log(ILog log)
+        {
+            this.logger = log;
+        }
+        public void Debug(object message)
+        {
+            this.logger.Debug(message);
+        }
+        public void Error(object message)
+        {
+            this.logger.Error(message);
+        }
+        public void Info(object message)
+        {
+            this.logger.Info(message);
+        }
+        public void Warn(object message)
+        {
+            this.logger.Warn(message);
+        }
+    }
+}
