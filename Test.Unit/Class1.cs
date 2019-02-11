67515a928a2caa17a5b3c3a82debf2576d733cf9
yuanyangfei <1207768558@qq.com>
2019-02-11 17:27:45 +0800

    测试

---
+++ Test.Unit/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using NUnit.Framework;
+using Think.Web.Controllers;
+
+namespace Test.Unit
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Should_Display()
+        {
+            var viewResult = (ViewResult)new AccountController().CheckLogin("yuan","123456");
+
+           // Assert.AreEqual( viewResult.ViewData.Model);
+
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            //// Act
+            //ViewResult result = controller.Index() as ViewResult;
+
+            //// Assert
+            //Assert.IsNotNull(result);
+        }
+        [Test]
+        public void IsValidFileName_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.PNG");
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ Test.Unit/LogAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Unit
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string name)
+        {
+            if (name.EndsWith(".png"))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+    }
+}
